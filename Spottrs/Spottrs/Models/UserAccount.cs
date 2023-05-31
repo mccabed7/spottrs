f1b915241912626495ee389d19a57aa06db19014
Declan <mccabed7@tcd.ie>
2023-05-31 21:58:42 +0100

    Continued work on UserAccount class

--- Spottrs/Spottrs/Models/UserAccount.cs
+++ Spottrs/Spottrs/Models/UserAccount.cs
@@ -1,7 +1,14 @@
-﻿namespace Spottrs.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using SQLite;
+
+namespace Spottrs.Models
 {
+
+    [Table("UserAccount")] //Might be able to remove this later on
     class UserAccount //lots more needed in this to get this to function
     {
+        [PrimaryKey, AutoIncrement, SQLite.Column("Id")]  //if doesn't work, swap SQLite with System......Schema
+        public int Id { get; set; }
         public string userName { get; set; }
         
     }
