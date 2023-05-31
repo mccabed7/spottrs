a961b8aa5b2229d94e36aceeba7fc39b0a684729
Declan <mccabed7@tcd.ie>
2023-05-31 20:41:17 +0100

    Added database files to project.

---
+++ Spottrs/Spottrs/Database/UserDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Spottrs.Database
+{
+    public class UserDatabase
+    {
+        string databasePath;
+        private SQLiteConnection connection;
+
+    }
+}
